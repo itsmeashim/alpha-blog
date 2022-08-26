ca896f006b9f5a609a4320fa735a85237a512b2f
ItsAshim <ashimqwe1@gmail.com>
2022-08-26 16:47:25 +0000

    Added: Show Action

---
+++ db/migrate/20220826131756_add_articles_timestamps.rb
@@ -0,0 +1,6 @@
+class AddArticlesTimestamps < ActiveRecord::Migration[6.1]
+  def change
+    add_column :articles, :created_at, :datetime
+    add_column :articles, :update_at, :datetime
+  end
+end
