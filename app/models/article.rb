11f5cfb91f6ebf01d7205a11b71a1698259e3c2f
ItsAshim <ashimqwe1@gmail.com>
2022-08-26 18:16:44 +0000

    Added: Edit and Add new Article function

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-    validates :title, presence: true, length: {minimum: 10}
+    validates :title, presence: true, length: {minimum: 5}
     validates :description, presence: true, length: {minimum: 8, maximum: 300}
 end
