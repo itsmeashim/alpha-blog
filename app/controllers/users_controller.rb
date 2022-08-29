f5671cbee07d997f347c0edaf695fe66e0d93930
ItsAshim <ashimqwe1@gmail.com>
2022-08-29 18:18:01 +0000

    Pagination Setup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,18 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id])
-        @articles = @user.articles
+        @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+    end
+
+    def index
+        @users = User.paginate(page: params[:page], per_page: 5)
     end
 
     def create
         @user = User.new(user_params)
         if @user.save
             flash[:notice] = "Hello dear #{@user.username}, Welcome to the Alpha Blog."
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'new'
         end
@@ -26,7 +30,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Your information updated successfully."
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'edit'
         end
