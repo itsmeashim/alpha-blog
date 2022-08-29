5076029b38e4f39295247f5bdf2383de94e92b96
ItsAshim <ashimqwe1@gmail.com>
2022-08-29 11:23:18 +0000

    Signup form Setup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,20 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:notice] = "Hello dear #{@user.username}, Welcome to the Alpha Blog."
+            redirect_to articles_path
+        else
+            render 'new'
+        end
+    end
+
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 end
